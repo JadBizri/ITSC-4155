658bee04acdd58e472bea5feb73646d509460b65
Isidro Casiano <isidroICP@outlook.com>
2024-03-18 17:32:59 -0400

    feat: database (#2)
    * feat(database): change post to item
    
    * feat(database): change name to title

--- src/server/api/routers/item.ts
+++ src/server/api/routers/item.ts
@@ -6,7 +6,7 @@ import {
   publicProcedure,
 } from "~/server/api/trpc";
 
-export const postRouter = createTRPCRouter({
+export const itemRouter = createTRPCRouter({
   hello: publicProcedure
     .input(z.object({ text: z.string() }))
     .query(({ input }) => {
@@ -16,21 +16,27 @@ export const postRouter = createTRPCRouter({
     }),
 
   create: protectedProcedure
-    .input(z.object({ name: z.string().min(1) }))
+    .input(z.object({ title: z.string().min(1) }))
     .mutation(async ({ ctx, input }) => {
       // simulate a slow db call
       await new Promise((resolve) => setTimeout(resolve, 1000));
 
-      return ctx.db.post.create({
+      return ctx.db.item.create({
         data: {
-          name: input.name,
+          title: input.title,
+          category: "OTHER",
+          price: 0,
+          description: "this is a description.",
+          image: "https://via.placeholder.com/150",
+          location: "Charlotte, NC",
+          campus: "UNCC",
           createdBy: { connect: { id: ctx.session.user.id } },
         },
       });
     }),
 
   getLatest: protectedProcedure.query(({ ctx }) => {
-    return ctx.db.post.findFirst({
+    return ctx.db.item.findFirst({
       orderBy: { createdAt: "desc" },
       where: { createdBy: { id: ctx.session.user.id } },
     });
