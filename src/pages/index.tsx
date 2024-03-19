5e3a4f441fcc45f7881ea212b244807b1bb6367a
JuanPablo2655 <personal@isidrocasiano.com>
2024-03-19 19:01:36 -0400

    fix: eslint warnings

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
-import { signIn, signOut, useSession } from 'next-auth/react';
+// import { signIn, signOut, useSession } from 'next-auth/react';
 import Head from 'next/head';
 
-import { api } from '~/utils/api';
-import { Button } from '~/components/ui/button';
+// import { api } from '~/utils/api';
+// import { Button } from '~/components/ui/button';
 import { SiteHeader } from '~/components/site-header';
 
 export default function Home() {
-	const hello = api.item.hello.useQuery({ text: 'from tRPC' });
+	// const hello = api.item.hello.useQuery({ text: 'from tRPC' });
 
 	return (
 		<>
@@ -22,24 +22,24 @@ export default function Home() {
 	);
 }
 
-function AuthShowcase() {
-	const { data: sessionData } = useSession();
+// function AuthShowcase() {
+// 	const { data: sessionData } = useSession();
 
-	const { data: secretMessage } = api.item.getSecretMessage.useQuery(
-		undefined, // no input
-		{ enabled: sessionData?.user !== undefined },
-	);
+// 	const { data: secretMessage } = api.item.getSecretMessage.useQuery(
+// 		undefined, // no input
+// 		{ enabled: sessionData?.user !== undefined },
+// 	);
 
-	return (
-		<div className="flex flex-col items-center justify-center gap-4">
-			<p className="text-center text-2xl text-white">
-				{sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-				{sessionData && <span>Email {sessionData.user?.email}</span>}
-				{secretMessage && <span> - {secretMessage}</span>}
-			</p>
-			<Button onClick={sessionData ? () => void signOut() : () => void signIn()}>
-				{sessionData ? 'Sign out' : 'Sign in'}
-			</Button>
-		</div>
-	);
-}
+// 	return (
+// 		<div className="flex flex-col items-center justify-center gap-4">
+// 			<p className="text-center text-2xl text-white">
+// 				{sessionData && <span>Logged in as {sessionData.user?.name}</span>}
+// 				{sessionData && <span>Email {sessionData.user?.email}</span>}
+// 				{secretMessage && <span> - {secretMessage}</span>}
+// 			</p>
+// 			<Button onClick={sessionData ? () => void signOut() : () => void signIn()}>
+// 				{sessionData ? 'Sign out' : 'Sign in'}
+// 			</Button>
+// 		</div>
+// 	);
+// }
