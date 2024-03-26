4c46421b7fd2ef7ad846431530cf6a75eb4576c3
JuanPablo2655 <personal@isidrocasiano.com>
2024-03-26 00:41:12 -0400

    feat: product prototype

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -1,11 +1,12 @@
+import Link from 'next/link';
 import { Auth } from './auth';
 import { MainNav } from './main-nav';
 import { ThemeToggle } from './theme-toggle';
 
 export function SiteHeader() {
 	return (
-		<header className="sticky top-0 flex justify-between p-6">
-			<h1>FlipMart</h1>
+		<header className="sticky top-0 flex flex-row justify-between p-6">
+			<Link href="/">FlipMart</Link>
 			<MainNav />
 			<div className="flex items-center gap-2">
 				<ThemeToggle />
