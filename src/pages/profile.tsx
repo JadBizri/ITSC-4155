be91c2e7e110317a5ddb7dc7dc7d201468cafed1
Jad Bizri <91910175+JadBizri@users.noreply.github.com>
2024-04-09 17:39:09 -0400

    Setup profile page

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -2,6 +2,11 @@ import { useSession } from 'next-auth/react';
 import { SiteHeader } from '~/components/site-header';
 import Head from 'next/head';
 import { ProfileImage } from '~/components/ui/profile-img';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '~/components/ui/tabs';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '~/components/ui/card';
+import { Button } from '~/components/ui/button';
+import { Input } from '~/components/ui/input';
+import { Label } from '~/components/ui/label';
 
 export default function Profile() {
 	const { data: sessionData } = useSession();
@@ -15,8 +20,36 @@ export default function Profile() {
 			</Head>
 			<div className="relative flex min-h-screen flex-col bg-white dark:bg-zinc-950">
 				<SiteHeader />
-
-				<ProfileImage imageUrl="https://picsum.photos/seed/picsum/4000" size="200px" />
+				<div className="mt-8 flex flex-col items-center space-y-10 text-center">
+					<ProfileImage imageUrl="https://picsum.photos/seed/picsum/4000" size="200px" />
+					{/* MUST ADD ACTUAL NAME HERE */}
+					<h1 className="text-4xl font-bold">Welcome [User's Name]!</h1>
+					<Tabs defaultValue="account" className="w-[400px]">
+						<TabsList>
+							<TabsTrigger value="account">Account</TabsTrigger>
+							<TabsTrigger value="listings">Listings</TabsTrigger>
+						</TabsList>
+						<TabsContent value="account">
+							<Card>
+								<CardHeader>
+									<CardTitle>Account</CardTitle>
+									<CardDescription>Make changes to your account here. Click save when you're done.</CardDescription>
+								</CardHeader>
+								<CardContent className="space-y-2">
+									<div className="space-y-1">
+										{/* MUST ADD ACTUAL NAME HERE */}
+										<Label htmlFor="name">Name</Label>
+										<Input id="name" defaultValue="YOUR NAME" />
+									</div>
+								</CardContent>
+								<CardFooter>
+									<Button>Save changes</Button>
+								</CardFooter>
+							</Card>
+						</TabsContent>
+						<TabsContent value="listings">Here are your listings</TabsContent>
+					</Tabs>
+				</div>
 			</div>
 		</>
 	);
