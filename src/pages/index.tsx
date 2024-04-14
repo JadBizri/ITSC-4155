d177cfe4d2f58c83793c0d3c135d5e8ff80c9b07
Isidro Casiano <personal@isidrocasiano.com>
2024-04-13 22:51:46 -0400

    feat: prototype (#4)
    * feat: prototype
    
    * feat: add search functionality and item page
    
    * feat: product prototype
    
    * feat: sidebar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
-// import { signIn, signOut, useSession } from 'next-auth/react';
 import Head from 'next/head';
+import { Item } from '~/components/item';
 
-// import { api } from '~/utils/api';
-// import { Button } from '~/components/ui/button';
+import { api } from '~/utils/api';
 import { SiteHeader } from '~/components/site-header';
+import { SiteSideBar } from '~/components/site-side-bar';
 
 export default function Home() {
-	// const hello = api.item.hello.useQuery({ text: 'from tRPC' });
+	const items = api.item.itemList.useQuery();
 
 	return (
 		<>
@@ -15,31 +15,13 @@ export default function Home() {
 				<meta name="description" content="ITSC 4155 Capstone Project" />
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
-			<div className="relative flex min-h-screen flex-col bg-white dark:bg-zinc-950">
+			<div className="relative flex flex-col">
 				<SiteHeader />
+				<div className="flex gap-10">
+					<SiteSideBar />
+					<div className="flex flex-wrap gap-7">{items.data?.map(item => <Item key={item.id} {...item} />)}</div>
+				</div>
 			</div>
 		</>
 	);
 }
-
-// function AuthShowcase() {
-// 	const { data: sessionData } = useSession();
-
-// 	const { data: secretMessage } = api.item.getSecretMessage.useQuery(
-// 		undefined, // no input
-// 		{ enabled: sessionData?.user !== undefined },
-// 	);
-
-// 	return (
-// 		<div className="flex flex-col items-center justify-center gap-4">
-// 			<p className="text-center text-2xl text-white">
-// 				{sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-// 				{sessionData && <span>Email {sessionData.user?.email}</span>}
-// 				{secretMessage && <span> - {secretMessage}</span>}
-// 			</p>
-// 			<Button onClick={sessionData ? () => void signOut() : () => void signIn()}>
-// 				{sessionData ? 'Sign out' : 'Sign in'}
-// 			</Button>
-// 		</div>
-// 	);
-// }
