fea7b8ece996d1dc425563408cb0da474dcdcd4d
Jiasheng Li <105992801+lixjia@users.noreply.github.com>
2024-05-01 01:26:58 -0400

    fixed phone verification UI (#28)
    * edited ui
    
    * added login
    
    * added logic
    
    ---------
    
    Co-authored-by: lixjia <JiaLi@Jias-MacBook-Pro.local>

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -47,17 +47,16 @@ export default function Profile() {
 	const handleSendOTP = async () => {
 		setLoading(true);
 		try {
-			//console.log(phoneNumber);
 			const response = await axios.post('/api/otp/otpSend', { phoneNumber });
-			//console.log(response);
 			if (response.status === 200) {
 				setOtpSent(true);
 			} else {
 				setOtpSent(false);
 			}
+			setOtpSent(true);
 		} catch (error) {
 			setOtpSent(false);
-			//alert('Failed to send OTP');
+			console.log(error);
 		} finally {
 			setLoading(false);
 		}
@@ -66,35 +65,22 @@ export default function Profile() {
 	const handleVerifyOTP = async () => {
 		setLoading(true);
 		try {
-			//console.log(otpCode);
 			const response = await axios.post('/api/otp/otpVerify', { phoneNumber, otpCode });
 			if (response.status === 200) {
 				setPhoneVerified(true);
-				setFeedbackMessage('Phone number verified successfully!');
-				//console.log(response)
-				//const now = new Date();
-				// await prisma.user.update({
-				// 	where: { id: sessionData.user.id },
-				// 	data: {
-				// 		phone: phoneNumber,
-				// 		phoneVerified: now
-				// 	}
-				// });
-				//console.log(prismaResp);
+				//**TODO** add phone into database
 			} else {
 				setPhoneVerified(false);
-				setFeedbackMessage('Verification failed. Please try again.');
 			}
 		} catch (error) {
 			setPhoneVerified(false);
-			setFeedbackMessage('Verification failed. Please try again.');
+			console.log(error);
 		} finally {
 			setLoading(false);
 		}
 	};
 	const handleOtpChange = async (otp: string) => {
 		setOtpCode(otp);
-		//console.log(otp);
 	};
 	return (
 		<>
@@ -138,52 +124,57 @@ export default function Profile() {
 														Send Verification Code
 													</Button>
 												</PopoverTrigger>
-												{otpSent ? (
-													<PopoverContent className="w-80">
-														<InputOTP maxLength={6} onChange={handleOtpChange}>
-															<InputOTPGroup>
-																<InputOTPSlot index={0} />
-																<InputOTPSlot index={1} />
-																<InputOTPSlot index={2} />
-															</InputOTPGroup>
-															<InputOTPSeparator />
-															<InputOTPGroup>
-																<InputOTPSlot index={3} />
-																<InputOTPSlot index={4} />
-																<InputOTPSlot index={5} />
-															</InputOTPGroup>
-														</InputOTP>
-														<Popover>
-															<PopoverTrigger asChild>
-																<Button
-																	variant="outline"
-																	className="m-auto"
-																	onClick={handleVerifyOTP}
-																	disabled={loading}
-																>
-																	Verify Code
-																</Button>
-															</PopoverTrigger>
-															<PopoverContent>
-																{loading ? (
-																	<p>Verifying...</p> // Display a loading message or spinner
-																) : phoneVerified ? (
-																	<p className="text-green-500">Phone number has been added and verified!</p>
-																) : (
-																	<p className="text-red-500">Fail to verify phone number</p>
-																)}
-															</PopoverContent>
-														</Popover>
-													</PopoverContent>
-												) : (
-													<PopoverContent>
-														{loading ? (
-															<p>Sending...</p> // Display a loading message or spinner
-														) : (
-															<p className="text-red-500">Fail to send OTP...please try again later.</p>
-														)}
-													</PopoverContent>
-												)}
+												<div className="flex flex-col items-center justify-center">
+													{otpSent ? (
+														<PopoverContent className="w-80">
+															<div className="flex flex-col items-center justify-center">
+																<InputOTP maxLength={6} onChange={handleOtpChange}>
+																	<InputOTPGroup>
+																		<InputOTPSlot index={0} />
+																		<InputOTPSlot index={1} />
+																		<InputOTPSlot index={2} />
+																	</InputOTPGroup>
+																	<InputOTPSeparator />
+																	<InputOTPGroup>
+																		<InputOTPSlot index={3} />
+																		<InputOTPSlot index={4} />
+																		<InputOTPSlot index={5} />
+																	</InputOTPGroup>
+																</InputOTP>
+																<Popover>
+																	<PopoverTrigger asChild>
+																		<Button
+																			variant="outline"
+																			className="mx-auto my-2"
+																			onClick={handleVerifyOTP}
+																			disabled={loading}
+																		>
+																			{' '}
+																			Verify Code{' '}
+																		</Button>
+																	</PopoverTrigger>
+																	<PopoverContent>
+																		{loading ? (
+																			<p>Verifying...</p>
+																		) : phoneVerified ? (
+																			<p className="text-green-500">Phone number has been added and verified!</p>
+																		) : (
+																			<p className="text-red-500">Fail to verify phone number</p>
+																		)}
+																	</PopoverContent>
+																</Popover>
+															</div>
+														</PopoverContent>
+													) : (
+														<PopoverContent>
+															{loading ? (
+																<p>Sending...</p>
+															) : (
+																<p className="text-red-500">Fail to send OTP...please try again later.</p>
+															)}
+														</PopoverContent>
+													)}
+												</div>
 											</Popover>
 										</CardFooter>
 									</Card>
