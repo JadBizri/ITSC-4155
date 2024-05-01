351ecf26e0d403c19d60196b11b30cf955df63d7
Isidro Casiano <personal@isidrocasiano.com>
2024-05-01 02:29:44 -0400

    feat: save phone on database (#31)

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -6,4 +6,13 @@ export const userRouter = createTRPCRouter({
 	deleteUser: protectedProcedure.mutation(async ({ ctx }) => {
 		return ctx.db.user.delete({ where: { id: ctx.session.user.id } });
 	}),
+
+	verifyUser: protectedProcedure
+		.input(z.object({ phone: z.string().min(10).max(10), phoneVerified: z.date() }))
+		.mutation(async ({ ctx, input }) => {
+			return ctx.db.user.update({
+				where: { id: ctx.session.user.id },
+				data: { phone: input.phone, phoneVerified: input.phoneVerified },
+			});
+		}),
 });
